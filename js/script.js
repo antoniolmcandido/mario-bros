223b0049ebe69140d60ad27b61a1b18c287d8d6e
Candido <antoniolmcandido@gmail.com>
2022-06-18 21:16:05 -0300

    adictional functions

--- js/script.js
+++ js/script.js
@@ -1,19 +1,48 @@
 const mario = document.querySelector(".mario")
 const pipe = document.querySelector(".pipe")
+const score = document.querySelector("#score")
+const gameOver = document.querySelector("#gameOver")
+const restart = document.querySelector("#restart")
+const themeSound = document.querySelector("#theme")
+const jumpSound = document.querySelector("#jump")
+const deathSound = document.querySelector("#death")
 
+// loop sound
+if (typeof themeSound.loop == "boolean") {
+    themeSound.loop = true
+} else {
+    themeSound.addEventListener("ended", () => {
+        this.currentTime = 0
+        this.play
+    }, false)
+}
+themeSound.play()
+
+// jump
 const jump = () => {
     mario.classList.add("jump")
+    jumpSound.play()
 
     setTimeout(() => {
         mario.classList.remove("jump")
     }, 500)
 }
 
+// score
+let intervalScore = null
+let playerScore = 0
+const scoreCounter = () => {
+    playerScore++
+    score.innerHTML = `Score <b>${playerScore}</b>`
+}
+intervalScore = setInterval(scoreCounter, 300)
+
+// loop of game
 const loopGame = setInterval(() => {
     const pipePosition = pipe.offsetLeft
-    const marioPosition = +window
+    const marioPosition = parseInt(window
         .getComputedStyle(mario)
-        .bottom.replace("px", "")
+        .bottom.replace("px", ""))
 
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
         pipe.style.animation = "none"
@@ -23,6 +52,11 @@ const loopGame = setInterval(() => {
         mario.style.bottom = `${marioPosition}px`
 
         mario.src = "img/over.png"
+        themeSound.pause()
+        deathSound.play()
+        clearInterval(intervalScore)
+        gameOver.style.display = "block"
+        restart.style.display = "block"
         mario.style.width = "75px"
         mario.style.marginLeft = "45px"
 
@@ -31,3 +65,5 @@ const loopGame = setInterval(() => {
 }, 10)
 
 document.addEventListener("keydown", jump)
+document.addEventListener("click", jump)
+document.addEventListener("touchstart", jump)
